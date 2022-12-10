ac508440a53092264aec90a554fb6effd848c560
iko4akov <iko4akov@gmai.com>
2022-12-10 13:14:47 +0300

    Created project

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+from utils import load_candidates, get_all, get_by_skill, get_by_pk
+
+adress_file = "candidates.json"
+
+data_candidates = load_candidates(adress_file)
+
+candidates = get_all(data_candidates)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_home():
+    home = ''
+    for k, v in candidates.items():
+        home += f"<pre>\n{v[0]} -\n{k}\n{v[1][0:]}\n</pre>\n"
+    return f"{home}"
+
+
+@app.route("/candidates/<int:pk>")
+def page_candidates(pk):
+    candidate, url = get_by_pk(pk, data_candidates)
+    return f"<img src='{url}'>\n<pre>\n{candidate['name']}\n{pk}\n{candidate['skills']}\n</pre>"
+
+
+@app.route("/skills/<skill>")
+def page_skills(skill):
+    return get_by_skill(skill, candidates)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5001)
---
+++ utils.py
@@ -0,0 +1,32 @@
+import json
+
+
+def load_candidates(file):
+    """ Load json file format in list."""
+    with open(file, encoding="utf-8") as f:
+        data_candidates = json.load(f)
+    return data_candidates
+
+
+def get_all(filename):
+    """return all candidates: name: [pk, skills]"""
+    candidates = {}
+    for i in range(len(filename)):
+        candidates[filename[i]['pk']] = [filename[i]["name"], "".join(filename[i]["skills"]).lower()]
+    return candidates
+
+
+def get_by_pk(pk, data_candidates):
+    """return name candidate of pk"""
+    candidate = data_candidates[pk]
+    url = f'{data_candidates[pk]["picture"]}'
+    return candidate, url
+
+
+def get_by_skill(skill_name, candidates):
+    """return names candidate of skills"""
+    valid_candidates = ''
+    for k, v in candidates.items():
+        if skill_name in v[1]:
+            valid_candidates += f"<pre>\n{v[0]} -\n{k}\n{v[1][0:]}\n</pre>\n"
+    return valid_candidates
